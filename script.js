8c3437cba45f82c5054cc6733de53d51c9c94354
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-19 13:26:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function activateSearch() {
+    var searchContainer = document.getElementById('searchContainer');
+    var searchInput = document.getElementById('searchInput');
+
+    // Add the 'active' class to the search container
+    searchContainer.classList.add('active');
+
+    // Set focus on the search input
+    searchInput.focus();
+}
